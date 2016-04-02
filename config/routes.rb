bc446a675737ee40f68baefb5e665cdee898e4a7
villetan <ville.tanskanen@helsinki.fi>
2016-04-02 22:50:08 +0300

    homepage started

--- config/routes.rb
+++ config/routes.rb
@@ -29,7 +29,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'users#index'
+   root 'frontpage#index'
 
 
   #Special routes
